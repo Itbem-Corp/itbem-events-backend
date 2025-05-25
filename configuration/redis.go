28b2807b1847ea5cc03f538febb4028e5a4ad0cb
Andres Becerra <andres.becerra@itbem.om>
2025-05-25 03:05:09 -0600

    third commit

--- configuration/redis.go
+++ configuration/redis.go
@@ -2,6 +2,7 @@ package configuration
 
 import (
 	"context"
+	"crypto/tls"
 	"events-stocks/models"
 	"log"
 	"strconv"
@@ -16,9 +17,10 @@ func InicializarRedis(cfg *models.Config) {
 	redisDb, _ := strconv.Atoi(cfg.RedisDb)
 
 	RedisClient = redis.NewClient(&redis.Options{
-		Addr:     cfg.RedisHost,     // ← ahora toma del cfg
-		Password: cfg.RedisPassword, // ← ahora toma del cfg
-		DB:       redisDb,
+		Addr:      cfg.RedisHost,     // ← ahora toma del cfg
+		Password:  cfg.RedisPassword, // ← ahora toma del cfg
+		DB:        redisDb,
+		TLSConfig: &tls.Config{},
 	})
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
