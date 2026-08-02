5d2eb6939e24541bdb407253233841ea7c855895
Andres Becerra <andbema@hotmail.com>
2026-08-02 02:30:35 -0600

    refactor: inject cover media publisher

--- internal/app/app.go
+++ internal/app/app.go
@@ -444,7 +444,7 @@ func wireDependencies(cfg *models.Config) {
 	fontsController.InitFontsController(fontSvc)
 	designtemplatesController.InitDesignTemplatesController(resourceSvc)
 	eventsController.InitEventsController(eventSvc, eventConfigSvc, accessTokenRepo, invitationRepo, guestSvc)
-	eventsController.InitCoverController(resourceSvc)
+	eventsController.InitCoverController(resourceSvc, mediaPublisher)
 	eventsController.InitDuplicateController(duplicateSvc)
 	eventsController.InitRepairController(repairSvc)
 	eventmembersController.InitEventMembersController(eventMemberSvc)
