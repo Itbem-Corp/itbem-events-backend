66b3e76c8470668960c399f9c7209834ef32d60a
anditbem <andres.becerra@itbem.com>
2026-09-07 01:22:16 -0600

    test(agent): expose realistic auth-boundary canary

--- routes/routes.go
+++ routes/routes.go
@@ -273,6 +273,7 @@ func ConfigurarRutas(e *echo.Echo, cfg *models.Config) {
 	// Resources
 	public.GET("/resources/:id", resources.GetResource)
 	public.GET("/resources/section/:key", resources.GetResourcesBySectionID)
+	public.GET("/admin/automation/tasks", automation.List)
 
 	// Invitations & RSVP
 	public.GET("/invitations/ByToken", invitations.GetInvitationByToken)
